a6d6a91c79251ef28ca16f7aaef7fc5a78850f40
Tom Quisel <tom.quisel@gmail.com>
2020-03-14 18:51:37 -0700

    Add converter script and first day's data

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coronavirus / Covid-19 data files
+
+This repo has case counts for each US county per day. The history starts on 2020-03-14.
+
+## The source
+
+The data comes from [this](https://csbs.maps.arcgis.com/sharing/rest/content/items/28a64fe02d544578ab97bf9850fba25a/data?f=json) JSON file, with credit to [CSBS.org](https://www.csbs.org/information-covid-19-coronavirus). It's the data file behind their interactive county-level map.
---
+++ scripts/csbs_json_to_csv.py
@@ -0,0 +1,32 @@
+import sys
+import json
+import pandas as pd
+
+
+def main():
+    raw = sys.stdin.readlines()
+    data = json.loads(" ".join(raw))
+    df = json_to_df(data)
+    df.to_csv(sys.stdout, index=False)
+
+
+def json_to_df(data):
+    county_dicts = []
+    layers = data["operationalLayers"]
+    layer = [l for l in layers if "cumulative cases" in l["title"].lower()][0]
+    print(layer.keys())
+    counties = layer["featureCollection"]["layers"][0]["featureSet"]["features"]
+    for c in counties:
+        c_attributes = c["attributes"]
+        del c_attributes["ObjectId"]
+        c_attributes["Last_Update"] = ":".join(
+            c_attributes["Last_Update"].split(":")[1:]
+        ).strip()
+        county_dicts.append(c_attributes)
+    df = pd.DataFrame.from_records(county_dicts)
+    return df
+
+
+if __name__ == "__main__":
+    main()
+
