7150fd7960110de2b8bc6165cc0a98d65572737a
Tom Quisel <tom.quisel@gmail.com>
2020-03-17 09:13:51 -0700

    Improve lat-lon, fatality rate

--- scripts/fixup_data_folder.py
+++ scripts/fixup_data_folder.py
@@ -24,32 +24,12 @@ def process_args():
 def clean_folder(folder):
     dfs = read_dfs_in_folder(folder)
 
-    for date, df in dfs.items():
-        # There shouldn't be any duplicates, this is just a safeguard
-        df.drop_duplicates(["County_Name", "State_Name"], inplace=True)
-        # Deaths are sometimes reported with a string like "2+1".
-        # Simplify to a single number (3)
-        df["Death"] = (
-            df["Death"]
-            .astype(str)
-            .str.split("+")
-            .map(lambda l: sum([int(s) for s in l]))
-        )
-        # Recalculate fatality rate as a proper rate [0-1]
-        df["Fatality_Rate"] = df["Death"] / df["Confirmed"]
+    do_basic_fixups(dfs)
+    index_by_county(dfs)
+    improve_lat_lons(dfs)
+    fixup_new_column(dfs)
 
-    for date, df in dfs.items():
-        # print(df)
-        yesterday = date - dt.timedelta(days=1)
-        if yesterday in dfs:
-            fixed_df = make_df_consistent_with_yesterday(df, dfs[yesterday])
-        else:
-            fixed_df = df.copy()
-            # "New" cases means nothing
-            fixed_df["New"] = None
-        filename = os.path.join(folder, date.isoformat() + ".csv")
-        fixed_df.to_csv(filename, index=False, float_format="%.7f")
-        print("Fixed up", filename)
+    write_csvs(folder, dfs)
 
 
 def read_dfs_in_folder(folder):
@@ -64,17 +44,69 @@ def read_dfs_in_folder(folder):
     return dfs
 
 
+def do_basic_fixups(dfs):
+    for df in dfs.values():
+        # fix some county name changes
+        df["County_Name"] = df["County_Name"].map(
+            {"Kauai County": "Kauai", "Maui County": "Maui"}
+        )
+        # There shouldn't be any duplicates, this is just a safeguard
+        df.drop_duplicates(["County_Name", "State_Name"], inplace=True)
+        # Deaths are sometimes reported with a string like "2+1".
+        # Simplify to a single number (3)
+        df["Death"] = (
+            df["Death"]
+            .astype(str)
+            .str.split("+")
+            .map(lambda l: sum([int(s) for s in l]))
+        )
+        # Recalculate fatality rate as a proper rate [0-1]
+        df["Fatality_Rate"] = (df["Death"] / df["Confirmed"]).clip(0, 1)
+
+
+def index_by_county(dfs):
+    # switch to county + state index for all dfs
+    for df in dfs.values():
+        df.set_index(["County_Name", "State_Name"], inplace=True, verify_integrity=True)
+
+
+def improve_lat_lons(dfs):
+    """Early lat/lon values were flawed. Use the most recent values in all dfs"""
+    newest_df = dfs[max(dfs.keys())]
+    for df in dfs.values():
+        df["Latitude"] = newest_df["Latitude"]
+        df["Longitude"] = newest_df["Longitude"]
+
+
+def fixup_new_column(dfs):
+    """Compute the "New" column from today's & yesterday's Confirmed values"""
+    for date, df in dfs.items():
+        yesterday = date - dt.timedelta(days=1)
+        if yesterday in dfs:
+            fixed_df = make_df_consistent_with_yesterday(df, dfs[yesterday])
+        else:
+            fixed_df = df.copy()
+            # If there was no data yesterday, "New" cases isn't meaningful
+            fixed_df["New"] = None
+        dfs[date] = fixed_df
+
+
 def make_df_consistent_with_yesterday(df, df_y):
     """This fixes the New column so that today = yesterday + New"""
     df = df.copy()
     df_y = df_y.copy()
-    df.set_index(["County_Name", "State_Name"], inplace=True, verify_integrity=True)
-    df_y.set_index(["County_Name", "State_Name"], inplace=True)
     df_y.reindex(df.index)
     df["New"] = df["Confirmed"] - df_y["Confirmed"]
     df.loc[df["New"].isnull(), "New"] = df["Confirmed"]
     df["New"] = df["New"].astype(int)
-    return df.reset_index()
+    return df
+
+
+def write_csvs(folder, dfs):
+    for date, df in dfs.items():
+        filename = os.path.join(folder, date.isoformat() + ".csv")
+        df.to_csv(filename, float_format="%.7f")
+        print("Fixed up", filename)
 
 
 if __name__ == "__main__":
