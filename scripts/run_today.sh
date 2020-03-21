e2004359bfa7ffe06b46a6294142c31e8303d2a8
Tom Quisel <tom.quisel@gmail.com>
2020-03-21 12:48:35 -0700

    Add first 3/21 update

--- scripts/run_today.sh
+++ scripts/run_today.sh
@@ -7,5 +7,5 @@ DATE=`date +"%Y-%m-%d"`
 cd ..
 curl $URL -o data/raw_scraped/$DATE.csv
 cat data/raw_scraped/$DATE.csv | python3 scripts/csbs_csv_clean.py > data/csv/$DATE.csv
-echo "CSV for $DATE writtein to data/csv/$DATE.csv"
+echo "CSV for $DATE written to data/csv/$DATE.csv"
 python3 scripts/fixup_data_folder.py data/csv
